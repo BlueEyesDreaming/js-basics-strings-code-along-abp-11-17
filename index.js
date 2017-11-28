2af877f4e760d8706aef16ac060e2bf9a4a5fc96
Ashton Reed <dreamergirl411@gmail.com>
2017-11-28 15:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Ashton';
 const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser[0]}`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
